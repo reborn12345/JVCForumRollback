2989e3bbdbcb13b6db3464340efcb2cbf55eea0b
reborn12345 <58338231+reborn12345@users.noreply.github.com>
2024-10-22 19:01:17 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,18 @@ module.exports = {
     collect: {
       url: ['https://www.jeuxvideo.com/forums.htm'],  // Change l'URL par celle que tu veux tester
       numberOfRuns: 3,
+      // Ajoute une étape avant l'audit
+      settings: {
+        emulatedFormFactor: 'desktop',
+        preAudits: [
+          {
+            // Un script pour accepter les cookies
+            script: `
+              document.querySelector('button.accepter-cookies').click();
+            `,
+          },
+        ],
+      },
     },
     upload: {
       target: 'temporary-public-storage',
